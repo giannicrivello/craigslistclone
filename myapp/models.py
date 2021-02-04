fce247e3da400bddee59e44628ccf7c1566f61c5
Gianni Crivello <gianniemperado@gmail.com>
2021-02-03 22:25:51 -0800

    initial commit

---
+++ myapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Search(models.Model):
+    def __str__(self):
+        return self.search  #human readable format in database
+
+    search = models.CharField(max_length=500)
+    created = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Searches'
+
+
+
